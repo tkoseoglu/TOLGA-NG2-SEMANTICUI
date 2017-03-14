804174ac6bddf2ec2e513bd5a498786d0732df41
Kemal Tolga Koseoglu <tolga.k@outlook.com>
2017-03-14 08:50:55 -0600

    Exporting tablepager component

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 export * from './ngsm-loader/ngsm-loader.component';
 export * from './ngsm-select/ngsm-select.component';
 export * from './ngsm-datepicker/ngsm-datepicker.component';
+export * from './ngsm-tablepager/ngsm-tablepager.component';
 export * from './app.component';
 export * from './app.module';
